b89283d691cc94e95982ed18243ec4f8e6d7a2ef
Frank Scrock <frank.scrock@cubits.com>
2015-11-28 14:18:45 +0100

    removed activerecord

--- businessapp/config/application.rb
+++ businessapp/config/application.rb
@@ -1,14 +1,17 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "active_model/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 Bundler.require(*Rails.groups)
 
 module Businessapp
   class Application < Rails::Application
-    config.generators do |g|
-      g.orm :mongo_mapper
-    end
+    config.generators{|g| g.orm :mongo_mapper}
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
